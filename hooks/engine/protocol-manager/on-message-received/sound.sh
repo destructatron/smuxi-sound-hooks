fe02ea67ca6de8bb8450c61d15247de97d4f59a9
Harley Richardson <destructatron2018@gmail.com>
2024-03-07 10:53:40 +0000

    Return when nick is detected in the received message
    This avoids a situation where the mention sound would play, followed by the normal channel message sound. Exit after playing the mention sound to avoid this.

--- hooks/engine/protocol-manager/on-message-received/sound.sh
+++ hooks/engine/protocol-manager/on-message-received/sound.sh
@@ -2,6 +2,7 @@
 if [[ $SMUXI_CHAT_TYPE == "Group" ]]; then
 if [[ $SMUXI_MSG == *$SMUXI_PROTOCOL_MANAGER_ME_ID* ]]; then
 play -qV0 /$HOME/smuxi-sounds/notify.wav
+exit 0
 fi
 play -qV0 $HOME/smuxi-sounds/channelmsg.wav
 elif [[ $SMUXI_CHAT_TYPE == "Person" ]]; then
